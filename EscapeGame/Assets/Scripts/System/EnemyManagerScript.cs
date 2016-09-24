aac88d836c7b3d87779cf02ba34cca6d0c60d077
temy13 <temy.poporofier00@gmail.com>
2016-09-24 16:16:02 +0900

    add enemy manager & spona

---
+++ EscapeGame/Assets/Scripts/System/EnemyManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManagerScript : MonoBehaviour {
+
+	public const int ENEMY_MAX = 3;
+	public const float GENERATE_TIME = 3.0f;
+	private float m_LeftTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_LeftTime += Time.deltaTime;
+		if (m_LeftTime > GENERATE_TIME) {
+
+		}
+	}
+}
