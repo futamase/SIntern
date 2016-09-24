4a51ec8b51eec2035cec9ec6299ab63608a80fc8
temy13 <temy.poporofier00@gmail.com>
2016-09-24 12:22:03 +0900

    fix PlayerBase

--- EscapeGame/Assets/Scripts/Player/PlayerBaseScript.cs
+++ EscapeGame/Assets/Scripts/Player/PlayerBaseScript.cs
@@ -8,7 +8,7 @@ public class PlayerBaseScript : MonoBehaviour {
 	private Vector3	m_Move = Vector3.zero;
 	private float m_Speed = 5.0f;
 	private const float	GRAVITY = 20f;			// 重力
-	private bool m_isUsing = true;
+	private bool m_IsUsing = true;
 
 
 	// Use this for initialization
@@ -23,18 +23,15 @@ public class PlayerBaseScript : MonoBehaviour {
 
 	void Move(){
 		float x = 0.0f;
-		if (this.m_isUsing) {
+		if (this.m_IsUsing) {
 			x = Input.GetAxis ("Horizontal") * m_Speed;
 		}
 		m_Move = new Vector3(x, m_Move.y , 0.0f);		// 左右上下のキー入力を取得し、移動量に代入.
-		m_Move = transform.TransformDirection(m_Move);
-		//m_Move *= m_Speed;		// 移動速度を乗算.
-		//m_Move.y += y;
 		m_Move.y -=  GRAVITY * Time.deltaTime;	// 重力を代入.
 		m_Controller.Move(m_Move * Time.deltaTime);	// キャラ移動.
 	}
 
 	public void ChangeCharacter(bool IsUsing){
-		this.m_isUsing = IsUsing;
+		this.m_IsUsing = IsUsing;
 	}
 }
