f1160fa678ab1b40ab1fa8bdae702ac832a8f656
temy13 <temy.poporofier00@gmail.com>
2016-09-25 09:23:15 +0900

    fix enemy

--- EscapeGame/Assets/Scripts/StageObjects/EnemyScript.cs
+++ EscapeGame/Assets/Scripts/StageObjects/EnemyScript.cs
@@ -8,6 +8,8 @@ public class EnemyScript : MonoBehaviour {
 	private float m_Speed = 10f;
 	private int m_Direction = 1;
 
+	private float timeLeft;
+
 	// Use this for initialization
 	void Start () {
 		m_Direction = -1;
@@ -21,18 +23,22 @@ public class EnemyScript : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		switch (collision.transform.tag) {
 		case "Block":
-			m_Direction *= -1;
-			this.transform.position += new Vector3 (m_Direction * m_Speed, 0, 0);
-			//this.transform.localScale = new Vector3 (this.transform.localScale.x * -1, 1, 1);
+			ChangeDirection ();
 			break;
 		case "FixedBlock":
-			m_Direction *= -1;
-			this.transform.position += new Vector3 (m_Direction * m_Speed, 0, 0);
-			//this.transform.localScale = new Vector3 (this.transform.localScale.x * -1, 1, 1);
+			ChangeDirection ();
 			break;
 		default:
 			break;
 		}
+	}
+
+	void ChangeDirection(){
+		
+		m_Direction *= -1;
+		Vector3 pos = this.transform.position;
+		this.transform.position = new Vector3 (pos.x + m_Direction * m_Speed, pos.y, 0);
+		this.transform.localScale = new Vector3 (this.transform.localScale.x * -1, 1, 1);
 	}
 
 	public void Reset(){
