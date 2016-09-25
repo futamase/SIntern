644e2ddecce74a3e0e45c9040d196bf9bf72b518
yuki futamase <misoni.s.8989@gmail.com>
2016-09-25 11:05:08 +0900

    effect and prefab

---
+++ EscapeGame/Assets/Resources/Scripts/Common/FXController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+
+//ＦＸ終了イベント用のイベントハンドラ
+public delegate void FXEventHandler();
+
+
+public class FXController : MonoBehaviour
+{
+
+    //components
+    private ParticleSystem[] mPartSystem;
+
+    //inspector
+    //再生速度
+    [SerializeField]
+    private float mPlaySpeed = 1.0f;
+
+    //寿命
+    [SerializeField]
+    private float mLifeTime = 2.0f;
+
+    //event
+    public event FXEventHandler EndEvent;
+
+    // Use this for initialization
+    void Start()
+    {
+        mPartSystem = GetComponentsInChildren<ParticleSystem>();
+
+        for (int i = 0; i < mPartSystem.Length; i++)
+            mPartSystem[i].playbackSpeed = mPlaySpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //寿命管理
+        mLifeTime -= Time.deltaTime;
+
+        if (mLifeTime < 0)
+        {
+            EndEvent();
+            Destroy(this.gameObject);
+        }
+    }
+}
